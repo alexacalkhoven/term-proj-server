3f55656501429408bc99197b0a18f382b337cff5
raduschirliu <radu.schirliu@gmail.com>
2020-04-08 13:26:18 -0600

    Fixed more brackets

--- src/main/model/CourseOffering.java
+++ src/main/model/CourseOffering.java
@@ -1,86 +1,73 @@
 package main.model;
+
 import java.util.ArrayList;
 
-public class CourseOffering
-{
+public class CourseOffering {
 	private int secNum;
 	private int secCap;
 	private int studentAmount;
 	private Course course;
 	private ArrayList<Registration> registrationList;
-	
-	public CourseOffering(int secNum, int secCap)
-	{
+
+	public CourseOffering(int secNum, int secCap) {
 		registrationList = new ArrayList<Registration>();
 		setStudentAmount(0);
 		setSecNum(secNum);
 		setSecCap(secCap);
 	}
-	
+
 	// ;)))))
 	// so
 	// LONG
 	// ooeoehoeo :^)
-	
-	public void addRegistration(Registration registration)
-	{
+
+	public void addRegistration(Registration registration) {
 		registrationList.add(registration);
 		setStudentAmount(getStudentAmount() + 1);
 	}
-	
-	public void removeRegistration(Registration registration)
-	{
+
+	public void removeRegistration(Registration registration) {
 		registrationList.remove(registration);
 		setStudentAmount(getStudentAmount() - 1);
 	}
 
-	public int getSecNum()
-	{
+	public int getSecNum() {
 		return secNum;
 	}
 
-	public void setSecNum(int secNumber)
-	{
+	public void setSecNum(int secNumber) {
 		this.secNum = secNumber;
 	}
 
-	public int getSecCap()
-	{
+	public int getSecCap() {
 		return secCap;
 	}
-	
-	public boolean isFull()
-	{
+
+	public boolean isFull() {
 		return studentAmount >= secCap;
 	}
 
-	public void setSecCap(int secCap)
-	{
+	public void setSecCap(int secCap) {
 		this.secCap = secCap;
 	}
-	
-	public void setCourse(Course course)
-	{
+
+	public void setCourse(Course course) {
 		this.course = course;
 	}
-	
-	public Course getCourse()
-	{
+
+	public Course getCourse() {
 		return this.course;
 	}
-	
-	public String toString()
-	{
+
+	public String toString() {
 		return "Section: " + getSecNum() + ", Spots: " + getStudentAmount() + " / " + getSecCap();
 	}
 
-	public int getStudentAmount()
-	{
+	public int getStudentAmount() {
 		return studentAmount;
 	}
 
-	public void setStudentAmount(int studentAmount)
-	{
+	public void setStudentAmount(int studentAmount) {
 		this.studentAmount = studentAmount;
 	}
 }
