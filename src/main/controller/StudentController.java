27a63a7a6c1c99df40b5968fb5c467fd2340ea9d
j0rd4n72 <jordan.kwan72@gmail.com>
2020-04-10 21:37:27 -0600

    added deleteCourse

--- src/main/controller/StudentController.java
+++ src/main/controller/StudentController.java
@@ -1,11 +1,13 @@
 package main.controller;
 
+import main.model.CourseCatalogue;
 import main.model.StudentList;
 
 public class StudentController {
 	private CommunicationManager comManager;
 	private StudentList studentList;
 	private int id;
+	private CourseCatalogue courseCatalogue;
 	
 	public StudentController(CommunicationManager comManager) {
 		this.comManager = comManager;
@@ -16,6 +18,11 @@ public class StudentController {
 	public void studentLogin(int id) {
 		this.id = id;
 	}
-	@HandleRequest("student.addRegCourse")
+	@HandleRequest("student.deleteCourse")
+	public void removeCourse(Object[] args) {
+		String name = (String)args[0];
+		int number = (Integer)args[1];
+		studentList.searchStudent(id).removeRegistration(courseCatalogue.getCourse(name, number));
+	}
 	
 }
