b9a8debaacbfc1bac9d4780c5d50da6a57e0c82d
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2020-04-18 10:26:27 -0600

    Removed comments

--- src/main/controller/CourseController.java
+++ src/main/controller/CourseController.java
@@ -39,7 +39,6 @@ public class CourseController {
 	
 	@HandleRequest("course.removeOffering")
 	public void removeOffering(Integer offeringId) throws InvalidRequestException {
-		// TODO: unregister students?
 		if (!courseCatalogue.removeCourseOffering(offeringId)) {
 			throw new InvalidRequestException("Failed to remove course offering");
 		}
@@ -70,7 +69,6 @@ public class CourseController {
 
 	@HandleRequest("course.remove")
 	public void removeCourse(Integer courseId) throws InvalidRequestException {
-		// TODO: unregister students???
 		if (!courseCatalogue.removeCourse(courseId)) {
 			throw new InvalidRequestException("Failed to remove course");
 		}
